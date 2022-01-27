3271812aaae99e20ff20ba1e275349c88f3ccb10
Ravin06 <77960307+Ravin06@users.noreply.github.com>
2022-01-27 16:08:47 +0800

    Create C02.py

---
+++ Moon Base/C02.py
@@ -0,0 +1,36 @@
+Briefing
+'''
+Next, let's take a look at something called variables, 
+a really simple programming concept (it's just a box with a label that can hold something), 
+but a thing you'll find very useful as you go on to do more advanced things.
+'''
+
+Solution
+
+# CHALLENGE 1: First create a variable called aString and assign it
+# the following text: Testing
+# Remember the format to assign data to a variable
+# is: variableName = "SomeValue"
+aString = "Testing"
+
+# CHALLENGE 2: Next create a variable called anInt and assign it the
+# following number: 899
+# Remember, you don't need quotes for numbers!
+anInt = 899
+
+# CHALLENGE 3: Next create a variable called aFloat and assign it the
+# following number: 12.55
+aFloat = 12.55
+
+# CHALLENGE 4: Next create a variable called aBool and assign it the
+# value False.
+aBool = False
+
+# CHALLENGE 5: Print the variables out in order of the challenge
+# number. i.e Challenge 1 first, then Challenge 2, and so on.
+print(aString)
+print(anInt)
+print(aFloat)
+print(aBool)
+
+
