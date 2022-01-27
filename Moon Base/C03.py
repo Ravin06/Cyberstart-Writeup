235764d1b60dbb19927725d7fc43220e12dbbde2
Ravin06 <77960307+Ravin06@users.noreply.github.com>
2022-01-27 16:12:49 +0800

    Create C03.py

---
+++ Moon Base/C03.py
@@ -0,0 +1,25 @@
+Briefing
+'''
+I hope you've been making notes agent as with this challenge you'll need to 
+take what you've just learnt about variables and try converting the types of data they contain.
+
+Oh, handy tip, if you make a bit of a mess with your code editor you can always click the reset 
+button on the bottom right. It'll refresh the editor and put back the original text so you can start again.
+'''
+
+Solution
+
+carInfo = "Number of Doors: "
+carDoors = 2
+
+# CHALLENGE 1: Convert carDoors to a string using type conversion.
+# Save it in a new variable: carDoorsString
+carDoorsString = str(carDoors)
+
+# CHALLENGE 2: Print out the number of doors in the car
+# using print(carInfo + carDoorsString)
+print(carInfo + carDoorsString)
+
+# Click "Back to my code" below to restore your code
+# or
+# Click "Submit code" above to get the flag
